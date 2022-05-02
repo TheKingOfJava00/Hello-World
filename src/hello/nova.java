6c494bb0f60ab2cf90eacafe2ff2f497db32a8b9
TheKingOfJava00 <104588512+TheKingOfJava00@users.noreply.github.com>
2022-05-02 22:12:41 +0200

    Verzija 5

---
+++ src/hello/nova.java
@@ -0,0 +1,18 @@
+package hello;
+
+public class nova {
+
+	public static void main(String[] args) {
+		
+		
+		int a=5;
+		
+		int b=7;
+		
+		int c=a+b;
+		
+		System.out.println("Hello");
+
+	}
+
+}
